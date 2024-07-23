a0bd899cddf608d9c3b7fbb56870cea63d58a914
Emma Davenport <davenport.physics@gmail.com>
2024-07-23 12:00:12 -0500

    Addition of Character sheet structs

--- char-validator/src/lib.rs
+++ char-validator/src/lib.rs
@@ -1,7 +1,8 @@
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-mod character_template;
+pub mod character_template;
+pub mod character_sheet;
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 pub fn add(left: usize, right: usize) -> usize {
