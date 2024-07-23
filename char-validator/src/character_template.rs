0af70edecab805be087ba3df62df2a55da118294
Emma Davenport <davenport.physics@gmail.com>
2024-07-23 11:45:06 -0500

    Addition of base character template structs

---
+++ char-validator/src/character_template.rs
@@ -0,0 +1,36 @@
+
+pub mod perk;
+pub mod weapon_proficiency;
+pub mod attributes;
+pub mod common;
+
+use perk::Perk;
+
+/**
+ * 
+ * How many points a character has for a particular attribute/skill/perk
+ * and how many points they can allocate to a single attribute/skill/perk
+ * 
+*/
+pub struct Points {
+    pub given_points: u64,
+    pub max_points_per_allotment: Option<u64>,
+}
+
+pub struct Allotment {
+    pub attributes: Points,
+    pub skills: Option<Points>,
+    pub perks: Option<Points>,
+}
+
+pub struct CharacterTemplate {
+    pub name: String,
+    pub version: [u8; 3],
+    pub description: String,
+
+    pub base_health: i32,
+    pub base_armor_class: i32,
+
+    pub allotments: Allotment,
+    pub perks: Option<Vec<Perk>>
+}
--- char-validator/src/lib.rs
+++ char-validator/src/lib.rs
@@ -1,6 +1,8 @@
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
+mod character_template;
+
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
