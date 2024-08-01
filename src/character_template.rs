f8710aa0a816ced60ea261fc996a7c91f5ca2a4b
Emma Davenport <davenport.physics@gmail.com>
2024-08-01 14:18:56 -0500

    Addition: perk validation

--- src/character_template.rs
+++ src/character_template.rs
@@ -20,8 +20,8 @@ use crate::character_sheet::CharacterSheet;
 */
 #[derive(Serialize, Deserialize)]
 pub struct Points {
-    pub given_points: u64,
-    pub max_points_per_allotment: Option<u64>,
+    pub given_points: i32,
+    pub max_points_per_allotment: Option<i32>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -51,32 +51,83 @@ impl CharacterTemplate {
     pub fn validate(&self, sheet: &CharacterSheet) -> Result<(), &'static str> {
 
         sheet.validate()?;
-
-        if !self.valid_template_name(sheet) {
-            return Err("Character template name mismatch");
-        }
-
-        if self.valid_version(sheet) {
-            return Err("Character template version mismatch");
-        }
+        self.valid_template_name(sheet)?;
+        self.valid_version(sheet)?;
+        self.valid_perk_allotment(sheet)?;
 
         Ok(())
 
     }
 
-    fn valid_template_name(&self, sheet: &CharacterSheet) -> bool {
+    fn valid_template_name(&self, sheet: &CharacterSheet) -> Result<(), &'static str> {
 
-        sheet.template.name == self.name
+        if sheet.template.name == self.name {
+            return Ok(());
+        }
+        
+        Err("Character template name mismatch")
 
     }
 
-    fn valid_version(&self, sheet: &CharacterSheet) -> bool {
+    fn valid_version(&self, sheet: &CharacterSheet) -> Result<(), &'static str> {
 
         if sheet.template.version.len() != self.version.len() {
-            return false;
+            return Err("Character template version mismatch");
         }
 
-        self.version.iter().eq(sheet.template.version.iter())
+        if self.version.iter().eq(sheet.template.version.iter()) {
+            return Ok(());
+        }
+
+        Err("Character template version mismatch")
+
+    }
+
+    fn valid_perk_allotment(&self, sheet: &CharacterSheet) -> Result<(), &'static str> {
+
+        if sheet.perks.is_none() {
+            return Ok(());
+        }
+
+        if self.perks.is_none() && sheet.perks.is_none() {
+
+            return Ok(());
+
+        } else if self.perks.is_none() && sheet.perks.is_some() {
+
+            if sheet.perks.as_ref().unwrap().is_empty() {
+                return Ok(());
+            } else {
+                return Err("Character template does not allow perks");
+            }
+
+        }
+
+        if let Some(perk_points) = &self.allotments.perks {
+
+            let template_perks = self.perks.as_ref().unwrap();
+            let sheet_perks    = sheet.perks.as_ref().unwrap();
+
+            let total_points: i32 = sheet_perks
+                .iter()
+                .map(|p| {
+
+                    template_perks
+                        .iter()
+                        .find(|tp| tp.name == *p)
+                        .unwrap()
+                        .point_cost
+
+                })
+                .sum();
+
+            if total_points > perk_points.given_points {
+                return Err("Character template does not allow enough perk points");
+            }
+
+        }
+
+        Ok(())
 
     }
 
