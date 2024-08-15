0a78bb60aa561d0855cca1a667379fbabcf31003
Emma Davenport <davenport.physics@gmail.com>
2024-08-15 09:25:24 -0500

    Addition: annotated configuration struct to be wasm compatible

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 
 # How to generate wasm
 
+## Requirements
+
+Install wasm-pack
+```sh
+cargo install wasm-pack
+```
+
+## Build
+
 ```sh
 wasm-pack build --target bundler
 ```
--- src/character_sheet/config.rs
+++ src/character_sheet/config.rs
@@ -2,25 +2,30 @@
 use std::sync::OnceLock;
 use serde::{Deserialize, Serialize};
 
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
 #[derive(Serialize, Deserialize, Copy, Clone)]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 pub struct CharacterNameConfig {
     pub min_length: i32,
     pub max_length: i32
 }
 
 #[derive(Serialize, Deserialize, Copy, Clone)]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 pub struct CharacterDescriptionConfig {
     pub max_length: i32
 }
 
 #[derive(Serialize, Deserialize, Copy, Clone)]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 pub struct CharacterSheetConfig {
     pub name: CharacterNameConfig,
     pub description: CharacterDescriptionConfig
 }
 
-const RAW_CHARACTER_SHEET_CONFIG: &str = include_str!("../../CharacterSheet.toml");
-
+static RAW_CHARACTER_SHEET_CONFIG: &str = include_str!("../../CharacterSheet.toml");
 static CHARACTER_SHEET_CONFIG: OnceLock<CharacterSheetConfig> = OnceLock::new();
 
 pub fn get_character_sheet_config() -> &'static CharacterSheetConfig {
